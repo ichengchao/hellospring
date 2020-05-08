76446caed95b6a619c37a6f07fa5d2d612b125eb
charles.chengc <charles.chengc@alibaba-inc.com>
2020-05-08 09:42:53 +0800

    remove keys

--- src/main/java/name/chengchao/hellospring/MyApplication.java
+++ src/main/java/name/chengchao/hellospring/MyApplication.java
@@ -8,11 +8,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan("name.chengchao.helloweb")
 public class MyApplication {
 
-
-    public static final String Aliyun_AccessKeyId = "LTAI4G7SCbPx6PudCAxHk5ap";
-
-    public static final String Aliyun_AccessKeySecret = "YUguDMwzIaCsbmi6SKmtcdguOMRwel";
-
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
