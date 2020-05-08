2b9dfae9bbb91b74d6479b2fa154a71e8d3baf79
charles.chengc <charles.chengc@alibaba-inc.com>
2020-05-08 09:39:02 +0800

    test key

--- src/main/java/name/chengchao/hellospring/MyApplication.java
+++ src/main/java/name/chengchao/hellospring/MyApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class MyApplication {
 
 
-    public static final String Aliyun_AccessKeyId = "bRrm85CCuCBy6d3a";
+    public static final String Aliyun_AccessKeyId = "LTAI4G7SCbPx6PudCAxHk5ap";
 
-    public static final String Aliyun_AccessKeySecret = "6AB07oXYXwO3b7hddzUnr8q53yd0DX";
+    public static final String Aliyun_AccessKeySecret = "YUguDMwzIaCsbmi6SKmtcdguOMRwel";
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
